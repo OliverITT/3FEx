7bdb79d2ff885811e8694c8cc72de4b4872d3115
Oliver  Rodriguez <oliverrodriguez@Macintosh.local>
2021-01-26 18:58:07 -0600

    ok UDPv4

--- functions.h
+++ functions.h
@@ -650,7 +650,8 @@ void *scanFlowIpv4UDP(void *valor)
                     std_biat = total_bpackets > 1 ? std_biat : NAN;
                     std_flowiat = (total_fpackets + total_bpackets) >1 ? std_flowiat : NAN;
                     mean_flowiat = (total_fpackets + total_bpackets) >1 ? mean_flowiat : NAN;
-                    fprintf(csv, "UDP,%d.%d.%d.%d->%d.%d.%d.%d,%d.%d.%d.%d,%u,%d.%d.%d.%d,%u,%f,%" PRId64 ",%" PRId64 ",%" PRId64 ",%" PRId64 ",%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,->,->,%f,%f,%f,%f,%d,%d,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%d,%f\n",
+                    fprintf(csv, "UDP,%f,%d.%d.%d.%d->%d.%d.%d.%d,%d.%d.%d.%d,%u,%d.%d.%d.%d,%u,%f,%" PRId64 ",%" PRId64 ",%" PRId64 ",%" PRId64 ",%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,->,->,%f,%f,%f,%f,%d,%d,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%d,%f\n",
+                            timestampInit,
                             ip_shost >> 24, ((ip_shost >> 16) & 0x0ff), ((ip_shost >> 8) & 0x0ff), ((ip_shost)&0x0ff),
                             ip_dhost >> 24, ((ip_dhost >> 16) & 0x0ff), ((ip_dhost >> 8) & 0x0ff), ((ip_dhost)&0x0ff),
                             ip_shost >> 24, (ip_shost >> 16) & 0x0ff, (ip_shost >> 8) & 0x0ff, (ip_shost)&0x0ff, ports.srcPort,
@@ -736,6 +737,7 @@ void *scanFlowIpv4UDP(void *valor)
 
                 //
                 incl_leng = (*(bffRawTrafic + (jump + 11)) << 24 | *(bffRawTrafic + (jump + 10)) << 16 | *(bffRawTrafic + (jump + 9)) << 8 | *(bffRawTrafic + (jump + 8)));
+                orig_len = (*(bffRawTrafic + (jump + 15)) << 24 | *(bffRawTrafic + (jump + 14)) << 16 | *(bffRawTrafic + (jump + 13)) << 8 | *(bffRawTrafic + (jump + 12)));
                 timestampTemp = *(bffRawTrafic + jump + 3) << 24 | *(bffRawTrafic + jump + 2) << 16 | *(bffRawTrafic + jump + 1) << 8 | *(bffRawTrafic + jump);
 
                 //busca paquetes distintos de cero
@@ -748,6 +750,7 @@ void *scanFlowIpv4UDP(void *valor)
                     jump += incl_leng + 16;
 
                     incl_leng = (*(bffRawTrafic + (jump + 11)) << 24 | *(bffRawTrafic + (jump + 10)) << 16 | *(bffRawTrafic + (jump + 9)) << 8 | *(bffRawTrafic + (jump + 8)));
+                    orig_len = (*(bffRawTrafic + (jump + 15)) << 24 | *(bffRawTrafic + (jump + 14)) << 16 | *(bffRawTrafic + (jump + 13)) << 8 | *(bffRawTrafic + (jump + 12)));
                     timestampTemp = *(bffRawTrafic + jump + 3) << 24 | *(bffRawTrafic + jump + 2) << 16 | *(bffRawTrafic + jump + 1) << 8 | *(bffRawTrafic + jump);
                 }
                 //check if ipv4
@@ -772,20 +775,20 @@ void *scanFlowIpv4UDP(void *valor)
                         ports.dstPort = *(bffRawTrafic + jump + 52) << 8 | *(bffRawTrafic + jump + 53);
                         //extraccion de featurs forward direccion primer paquete encontrado de la sesion a scanear
                         total_fpackets++;
-                        total_fpktl = incl_leng;
+                        total_fpktl = orig_len;
                         //
-                        min_fpktl = incl_leng;
+                        min_fpktl = orig_len;
                         //
-                        max_fpktl = incl_leng;
+                        max_fpktl = orig_len;
                         //
-                        vector_std_fpktl.push_back(incl_leng);
+                        vector_std_fpktl.push_back(orig_len);
                         //
                         //
-                        min_flowpktl = max_flowpktl = incl_leng;
+                        min_flowpktl = max_flowpktl = orig_len;
                         //
-                        mean_flowpktl = incl_leng;
+                        mean_flowpktl = orig_len;
                         //
-                        vector_std_flowpktl.push_back(incl_leng);
+                        vector_std_flowpktl.push_back(orig_len);
                         //
                         //marca el paquete time stamp segundo en 0 para indicar que ya fue procesado ese paquete
                         mark(jump);
@@ -818,12 +821,12 @@ void *scanFlowIpv4UDP(void *valor)
 
                         //extraccion de caracteristicas forward direccion
                         total_fpackets++;
-                        total_fpktl += incl_leng;
+                        total_fpktl += orig_len;
                         //
-                        min_fpktl = incl_leng < min_fpktl ? incl_leng : min_fpktl;
-                        max_fpktl = incl_leng > max_fpktl ? incl_leng : max_fpktl;
+                        min_fpktl = orig_len < min_fpktl ? orig_len : min_fpktl;
+                        max_fpktl = orig_len > max_fpktl ? orig_len : max_fpktl;
                         //
-                        vector_std_fpktl.push_back(incl_leng);
+                        vector_std_fpktl.push_back(orig_len);
                         //
                         iptTemp = timestampTemp - timestampPrevForward;
                         if (!iatForwardState)
@@ -840,13 +843,13 @@ void *scanFlowIpv4UDP(void *valor)
                         vector_std_fiat.push_back(iptTemp);
                         //
                         //
-                        min_flowpktl = incl_leng < min_flowpktl ? incl_leng : min_flowpktl;
+                        min_flowpktl = orig_len < min_flowpktl ? orig_len : min_flowpktl;
                         //
-                        max_flowpktl = incl_leng > max_flowpktl ? incl_leng : max_flowpktl;
+                        max_flowpktl = orig_len > max_flowpktl ? orig_len : max_flowpktl;
                         //
-                        mean_flowpktl += incl_leng;
+                        mean_flowpktl += orig_len;
                         //
-                        vector_std_flowpktl.push_back(incl_leng);
+                        vector_std_flowpktl.push_back(orig_len);
                         //
                         min_flowiat = iptTemp < min_flowiat ? iptTemp : min_flowiat;
                         //
@@ -874,11 +877,11 @@ void *scanFlowIpv4UDP(void *valor)
 
                         //extraccion de caracteristicas en backward direccion
                         total_bpackets++;
-                        total_bpktl += incl_leng;
+                        total_bpktl += orig_len;
                         //
 
                         //
-                        vector_std_bpktl.push_back(incl_leng);
+                        vector_std_bpktl.push_back(orig_len);
                         //
                         vector_std_flowiat.push_back(iptTemp);
                         //
@@ -891,7 +894,7 @@ void *scanFlowIpv4UDP(void *valor)
                         {
                             iatBackwardState = true;
                             timestampPrevBackware = timestampTemp;
-                            min_bpktl = incl_leng;
+                            min_bpktl = orig_len;
                             min_biat = timestampTemp;
                         }
                         else
@@ -907,18 +910,18 @@ void *scanFlowIpv4UDP(void *valor)
                             vector_std_biat.push_back(iptTemp);
                         }
 
-                        min_bpktl = incl_leng < min_bpktl ? incl_leng : min_bpktl;
-                        max_bpktl = incl_leng > max_bpktl ? incl_leng : max_bpktl;
+                        min_bpktl = orig_len < min_bpktl ? orig_len : min_bpktl;
+                        max_bpktl = orig_len > max_bpktl ? orig_len : max_bpktl;
                         timestampPrevBackware = timestampTemp;
                         //
                         //
-                        min_flowpktl = incl_leng < min_flowpktl ? incl_leng : min_flowpktl;
+                        min_flowpktl = orig_len < min_flowpktl ? orig_len : min_flowpktl;
                         //
-                        max_flowpktl = incl_leng > max_flowpktl ? incl_leng : max_flowpktl;
+                        max_flowpktl = orig_len > max_flowpktl ? orig_len : max_flowpktl;
                         //
-                        mean_flowpktl += incl_leng;
+                        mean_flowpktl += orig_len;
                         //
-                        vector_std_flowpktl.push_back(incl_leng);
+                        vector_std_flowpktl.push_back(orig_len);
                         //
                         //marcar el paquete
                         mark(jump);
--- makefile
+++ makefile
@@ -13,8 +13,8 @@ compile:
 run:
 	#./generador prueba.pcap malo.pcap hola.csv holaipts.txt
 	#./generador prueba.pcap hola.csv holaipts.txt
-	#./generador tftp_rrq.pcap hola.csv holaipts.txt
-	./generador nb6-http.pcap hola.csv holaipts.txt
+	./generador tftp_rrq.pcap hola.csv holaipts.txt
+	#./generador nb6-http.pcap hola.csv holaipts.txt
 	#./generador sesioncompleta1.pcap malo.pcap hola.csv holaipts.txt
 	#/generador tcpdump1.out hola.csv holaipts.txt
 	#./generador idprueba.pcap hola.csv holaipts.txt

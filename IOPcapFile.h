cd605553b325e0cf375aaed31a758b00c23e6a15
Oliver  Rodriguez <oliverrodriguez@Macintosh.local>
2021-02-15 19:31:42 -0600

    ok OI FILE and packet abstract

--- IOPcapFile.h
+++ IOPcapFile.h
@@ -4,19 +4,19 @@
 #include <stdio.h>
 #include <iostream>
 
-void *getNextPacket(FILE &badTrafic, Packet_pcap *&packet_pcap)
+void *getNextPacket(FILE &FileTrafic, Packet_pcap *&packet_pcap)
 {
-    if (ftell(&badTrafic) == 0)
+    if (ftell(&FileTrafic) == 0)
     {
-        fseek(&badTrafic, 24L, SEEK_SET);
+        fseek(&FileTrafic, 24L, SEEK_SET);
     }
     packet_pcap = new Packet_pcap;
 
-    if (fread(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &badTrafic))
+    if (fread(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic))
     {
         /* code */
 
-        if (!fread(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &badTrafic))
+        if (!fread(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic))
         {
             printf("unknown file");
             return NULL;
@@ -25,73 +25,156 @@ void *getNextPacket(FILE &badTrafic, Packet_pcap *&packet_pcap)
         {
         case __IPV4:
 
-            if (!fread(&packet_pcap->ip_layer.ipv4Header, sizeof(Ipv4Header), 1, &badTrafic))
+            if (!fread(&packet_pcap->ip_layer.ipv4Header, sizeof(Ipv4Header), 1, &FileTrafic))
             {
                 return NULL;
             }
             switch (packet_pcap->ip_layer.ipv4Header.protocol)
             {
             case _TCP:
-                if (!fread(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &badTrafic))
+                if (!fread(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &FileTrafic))
                 {
                     return NULL;
                 }
-                packet_pcap->tipe =IPV4_TCP;
-                //fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40), SEEK_CUR);
-                //packet_pcap->payload = new uint8_t[8];
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40), SEEK_CUR);
+                packet_pcap->tipe = IPV4_TCP;
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 40)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
             case _UDP:
-                if (!fread(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &badTrafic))
+                if (!fread(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &FileTrafic))
                 {
                     return NULL;
                 }
                 packet_pcap->tipe = IPV4_UDP;
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 20 - 8), SEEK_CUR);
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 20 - 8), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 20 - 8)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 20 - 8), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
 
             default:
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 20), SEEK_CUR);
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 20), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 20)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 20), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
             }
             break;
         case __IPV6:
-            if (!fread(&packet_pcap->ip_layer.ipv6Header, sizeof(Ipv6Header), 1, &badTrafic))
+            if (!fread(&packet_pcap->ip_layer.ipv6Header, sizeof(Ipv6Header), 1, &FileTrafic))
             {
                 return NULL;
             }
             switch (packet_pcap->ip_layer.ipv6Header.nextHeader)
             {
             case _TCP:
-                if (!fread(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &badTrafic))
+                if (!fread(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &FileTrafic))
                 {
                     return NULL;
                 }
                 packet_pcap->tipe = IPV6_TCP;
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40 - 20), SEEK_CUR);
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40 - 20), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 40 - 20)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40 - 20), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
             case _UDP:
-                if (!fread(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &badTrafic))
+                if (!fread(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &FileTrafic))
                 {
                     return NULL;
                 }
                 packet_pcap->tipe = IPV6_UDP;
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40 - 8), SEEK_CUR);
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40 - 8), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 40 - 8)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40 - 8), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
 
             default:
-                fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40), SEEK_CUR);
+                //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14 - 40), SEEK_CUR);
+                packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14 - 40)];
+                if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40), &FileTrafic))
+                {
+                    return NULL;
+                }
                 break;
             }
             break;
         default:
-            fseek(&badTrafic, (packet_pcap->packetHeader.incl_len - 14), SEEK_CUR);
-           packet_pcap->tipe = OTDER_TRAFIC;
+            //fseek(&FileTrafic, (packet_pcap->packetHeader.incl_len - 14), SEEK_CUR);
+            packet_pcap->tipe = OTDER_TRAFIC;
+            packet_pcap->payload = new uint8_t[(packet_pcap->packetHeader.incl_len - 14)];
+            if (!fread(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14), &FileTrafic))
+            {
+                return NULL;
+            }
             break;
         }
         return (void *)1;
     }
 
     return NULL;
+}
+void writePacket(FILE &FileTrafic, Packet_pcap *&packet_pcap, ...)
+{
+
+    switch (packet_pcap->tipe)
+    {
+    case IPV4_TCP:
+        fwrite(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ip_layer.ipv4Header, sizeof(Ipv4Header), 1, &FileTrafic);
+        fwrite(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &FileTrafic);
+        fwrite(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40), &FileTrafic);
+        break;
+    case IPV4_UDP:
+        fwrite(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ip_layer.ipv4Header, sizeof(Ipv4Header), 1, &FileTrafic);
+        fwrite(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &FileTrafic);
+        fwrite(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 20 - 8), &FileTrafic);
+        break;
+    case IPV6_TCP:
+        fwrite(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ip_layer.ipv6Header, sizeof(Ipv6Header), 1, &FileTrafic);
+        fwrite(&packet_pcap->proto.tcpHeader, sizeof(TCPheader), 1, &FileTrafic);
+        fwrite(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40 - 20), &FileTrafic);
+        break;
+    case IPV6_UDP:
+        fwrite(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ip_layer.ipv6Header, sizeof(Ipv6Header), 1, &FileTrafic);
+        fwrite(&packet_pcap->proto.uDPheader, sizeof(UDPheader), 1, &FileTrafic);
+        fwrite(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14 - 40 - 8), &FileTrafic);
+        break;
+    default:
+        fwrite(&packet_pcap->packetHeader, sizeof(PcapPackHeader), 1, &FileTrafic);
+        fwrite(&packet_pcap->ethernetHeader, sizeof(EtherHeader), 1, &FileTrafic);
+        fwrite(packet_pcap->payload, sizeof(uint8_t), (packet_pcap->packetHeader.incl_len - 14), &FileTrafic);
+        break;
+    }
+    delete packet_pcap;
+}
+void readHeaderPcapFile(FILE &FileTrafic, PacapFileHeader *&pacapFileHeader)
+{
+    pacapFileHeader = new PacapFileHeader;
+    fread(pacapFileHeader, sizeof(PacapFileHeader), 1, &FileTrafic);
+}
+void writeHeaderPcapFile(FILE &FileTrafic,PacapFileHeader *&pacapFileHeader)
+{
+    fwrite(pacapFileHeader, sizeof(PacapFileHeader), 1, &FileTrafic);
 }
 #endif

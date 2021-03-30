12ca0ca73996650c6656ceafc5dba8043b6c5465
OliverITT <olgirodriguezad@ittepic.edu.mx>
2021-03-30 12:59:44 -0600

    Parsing command-line arguments and flags

--- splitTrafic.cpp
+++ splitTrafic.cpp
@@ -1,9 +1,58 @@
 #include "scanBadTrafic.h"
+const char banner[] = {"-r  Input pcap raw file\n-b  Input pcap alert file\n-o  Output pcap freeatack packets"};
+#define ERROR 1
 int main(int argc, char **argv)
 {
-    FILE *rawTrafic = fopen(argv[1], "rb");
-    FILE *badTrafic = fopen(argv[2], "rb");
-    FILE *freeAnomaliTrafic = fopen(argv[3], "wb");
+    FILE *rawTrafic;// = fopen(argv[1], "rb");
+    FILE *badTrafic;// = fopen(argv[2], "rb");
+    FILE *freeAnomaliTrafic;// = fopen(argv[3], "wb");
+    if (argc < 7)
+    {
+        printf("%s", banner);
+        return 1;
+    }
+    if (argc > 6)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string arg = argv[i];
+            if (arg == "-r")
+            {
+                rawTrafic = fopen((char *)argv[i + 1], "rb");
+                if (!rawTrafic)
+                {
+                    printf("no open file: %s", argv[i + 1]);
+                    return ERROR;
+                }
+                if (!isPcapFile(*rawTrafic))
+                {
+                    return ERROR;
+                }
+            }
+            if (arg == "-b")
+            {
+                badTrafic = fopen((char *)argv[i + 1], "rb");
+                if (!badTrafic)
+                {
+                    printf("no open file: %s", argv[i + 1]);
+                    return ERROR;
+                }
+                if (!isPcapFile(*badTrafic))
+                {
+                    return ERROR;
+                }
+            }
+            if (arg == "-o")
+            {
+                freeAnomaliTrafic = fopen(argv[i + 1], "wb");
+                if (!freeAnomaliTrafic)
+                {
+                    printf("no open file: %s", argv[i + 1]);
+                    return ERROR;
+                }
+            }
+        }
+    }
     if (!rawTrafic || !badTrafic || !freeAnomaliTrafic)
     {
         printf("unknown file\n");
@@ -27,3 +76,8 @@ int main(int argc, char **argv)
     }
     return 0;
 }
+
+/*
+
+    
+*/
